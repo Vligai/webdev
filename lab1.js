6b4301af2c31776eeff99d1c76fc6c53e3c13258
vligai <vligai@stevens.edu>
2016-08-31 15:56:36 -0400

    base

---
+++ lab1.js
@@ -0,0 +1,21 @@
+//Vladislav Ligai
+//Lab 1
+function sumOfSquares(num1,num2,num3){
+	return 0;
+}
+
+function sayHelloTo(firstName, lastName, title){
+	return 0;
+}
+
+function cupsOfCoffee(howManyCups){
+	return 0;
+}
+
+function occurrencesOfSubstring(fullString, substring){
+	return 0;
+}
+
+function randomizeSentences(paragraph){
+	return 0;
+}
