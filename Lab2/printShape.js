31c395d2cbf43cc4f1a2c11961bfe53a1248bff8
vligai <vligai@stevens.edu>
2017-09-14 19:35:28 -0400

    cleaned up lab 2

--- Lab2/printShape.js
+++ Lab2/printShape.js
@@ -1,6 +1,8 @@
 /*
 Vladislav Ligai
 CS546 lab 2 
+I haven't reslly used any dependencies, but still installed 'prompt' as per the calculator example
+to run: npm start
 */
 //Function taken from the lecture code https://github.com/Stevens-CS546/CS-546-WS-Summer-1/blob/master/Lecture%20Code/lecture_02/calculator_module_example/calculator.js
 function checkIsProperNumber(val, variableName) {
@@ -13,6 +15,7 @@ module.exports = {
 	
 	//Printing Triangles
 	triangle: function(lines) {
+		//check for valid input
 		checkIsProperNumber(lines, "Number of Lines");
 		if (lines <= 0)
 			throw ("Please enter a number that is more than 0!");
@@ -23,14 +26,17 @@ module.exports = {
 			var c = 0;
 			for (;j >= i; j--)
 				result = result + " ";
+			//left side
 			result = result + "\/";
 			for (;c < i; c++)
 			{
+				//base of the triangle
 				if(i==lines-1)
 					result = result + "--";
 				else 
 					result = result + "  ";			
 			}
+			//right side
 			result = result + "\\";
 			console.log(result);
 		}
@@ -39,7 +45,7 @@ module.exports = {
 	
 	//Printing Squares
 	square: function(lines) {
-		
+		//check for valid input
 		checkIsProperNumber(lines, "Number of Lines");
 		if (lines < 2)
 			throw ("Number of lines provided is less than 2!");
@@ -48,41 +54,46 @@ module.exports = {
 			var result = "";
 			var j = lines - 1;
 			var c = 0;
+			//left side
 			result = result + "|";
 			while(c < lines)
 			{
-				if(i == 0 || i == j)
+				//horizontal edges
+				if(i == j || i == 0 )
 					result = result + "-";
 				else 
 					result = result + " ";
 				c++;
 			}
+			//right side
 			result = result + "|";
 			console.log(result);
-			//return result;
 		}
 		console.log('\n');
 	},
 	
 	//Printing Rhombuses
 	rhombus: function (lines) {
+		//check for valid input
         checkIsProperNumber(lines, "Number of Lines");
 		if (lines < 2)
 			throw ("Number of lines provided is less than 2!");
 		if (lines % 2 != 0)
 			throw ("Number of lines provided is not even!");
 		var x = 1;
-        for (i = 0; i < lines; i++) {
+        for (i = 0; i < lines; i++) 
+		{
 			// top half of rhombus
 			if (i < lines/2)
 			{
 				var output = "";
 				for (j = 0; j < lines/2 - i - 1; j++)
 				output = output + " ";
+				//left side
 				output = output + "\/";
 				for (c = 0; c <= i; c++) 
 				{
-					if (i == 0 && c == 0)
+					if (c == 0 && i == 0)
 						output = output + "-"
 					else if (i == 1) 
 					{
@@ -94,6 +105,7 @@ module.exports = {
 					else
 						output = output + "  ";
 				}
+				//right side
 				output = output + "\\";
 				console.log(output);
 			}
@@ -103,12 +115,12 @@ module.exports = {
 				var output2 = "";
 				for (k = 1; k < x; k++)
 					output2 = output2 + " ";
+				//left side
 				output2 = output2 + "\\";
 				var con = 0;
-				for (ii = lines / 2 - x; ii >= 0; ii--) 
+				for (ii = 0; ii <= lines / 2 - x; ii++) 
 				{
-					//console.log(x);
-					if (x == lines / 2 && ii == 0)
+					if (ii == 0 && x == lines / 2)
 						output2 = output2 + "-";
 					else if (x == lines / 2 - 1) 
 					{
@@ -124,6 +136,7 @@ module.exports = {
 						output2 = output2 + "  ";
 				}
 				x++;
+				//right side
 				output2 = output2 + "\/";
 				console.log(output2);
 			}
